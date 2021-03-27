c50808217140ffe6ca7286c8a6363502ddf516a4
rahul975337 <tilwani03@gmail.com>
2021-03-28 03:43:24 +0530

    dv

--- src/components/Navbars/HorizontalNav/HorizontalNav.js
+++ src/components/Navbars/HorizontalNav/HorizontalNav.js
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 import "./HorizontalNav.css";
 import { Close } from "@material-ui/icons";
 import NavTab from "./NavTab";
+import {
+  VscEllipsis,
+  VscGitCompare,
+  VscSplitHorizontal,
+} from "react-icons/vsc";
 function HorizontalNav() {
   return (
     <nav className="hell">
@@ -10,8 +15,10 @@ function HorizontalNav() {
       <NavTab tab_name="projects.css" routeUrl="/projects" />
       <NavTab tab_name="skills.sql" routeUrl="/skills" />
       <NavTab tab_name="experience.sql" routeUrl="/experience" />
-
       <NavTab tab_name="about.md" routeUrl="/about" />
+      <VscGitCompare className="compare-icon" />
+      <VscSplitHorizontal className="split-screen-icon" />
+      <VscEllipsis className="dot-icon" />
     </nav>
   );
 }
