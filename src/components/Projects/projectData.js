dfcf66f17aec620d98820003cfea7df0e32dbadf
rahul975337 <tilwani03@gmail.com>
2021-03-28 03:35:19 +0530

    updated projects

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -9,30 +9,24 @@ const projectData = [
   {
     id: 2,
     project_name: "Reactbnb",
-    project_url: "https://reactboook-rt.netlify.app/",
-    project_description: "Facebook Clone in Reactjs",
-    project_tech: "Reactjs, Firebase, Material UI",
+    project_url: "https://reactbnb-rt.netlify.app/",
+    project_description: "Airbnb Clone in Reactjs",
+    project_tech: "Reactjs, Material UI",
   },
   {
     id: 3,
-    project_name: "Codepen",
-    project_url: "https://reactboook-rt.netlify.app/",
-    project_description: "Facebook Clone in Reactjs",
-    project_tech: "Reactjs, Firebase, Material UI",
+    project_name: "Covid Tracker",
+    project_url: "https://covid-tracker-rt.netlify.app/",
+    project_description:
+      "Covid Tracker with worldwide live cases update, Country wise filtering option and map representation",
+    project_tech: "Reactjs,Leaflet Maps, Covid19 API, Material UI",
   },
   {
     id: 4,
-    project_name: "Crypto",
-    project_url: "https://reactboook-rt.netlify.app/",
-    project_description: "Facebook Clone in Reactjs",
-    project_tech: "Reactjs, Firebase, Material UI",
-  },
-  {
-    id: 5,
-    project_name: "Portfolio",
-    project_url: "https://reactboook-rt.netlify.app/",
-    project_description: "Facebook Clone in Reactjs",
-    project_tech: "Reactjs, CSS, Material UI",
+    project_name: "Codepen",
+    project_url: "https://codepen-rt.netlify.app/",
+    project_description: "Codepen Clone in Reactjs",
+    project_tech: "Reactjs, Material UI",
   },
 ];
 
